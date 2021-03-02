eca62f077b47d0db990adfba7cbaff41d0fbc599
Yashraj Desai <yashrajdesai30@gmail.com>
2021-03-02 09:44:49 +0530

    Updated the project structure

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -6,8 +6,9 @@ module.exports = async function(deployer) {
   await deployer.deploy(DaiTokenMock);
   const tokenMock = await DaiTokenMock.deployed()
   // Mint 1,000 Dai Tokens for the deployer
+
   await tokenMock.mint(
-    '0x3AEDC2FaF50742Be057436c56B4E087e542698a0',
+    '0xa6B26659a48Aef654e4E567689E8183d8A66e755',
     '1000000000000000000000'
   )
   
