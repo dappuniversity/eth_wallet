d9ba063f0e544f0cbd0297478844deae949f187e
Mihir <ranemihir3@gmail.com>
2021-03-19 11:26:45 +0530

    Added jumbotron and message for crownfunding

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { useHistory } from "react-router-dom";
+import { Jumbotron, Container, Card, Button, Col, Row} from 'react-bootstrap';
 
 function Home() {
+
+    const history = useHistory();
+
+    const routeChange = () =>{ 
+        let path = `/payment`; 
+        history.push(path);
+    }
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div> 
+            <div className="statistics-strip">
+                View our statistics page and then donate on this public key 0x7691B75d3aAED69F63d15bDe4490d926CAD94C93
+                <Button className="mx-4 my-2 statistics-button" variant="info" onClick={routeChange}>Statistics</Button>    
+            </div> 
+                    
+            <Jumbotron fluid className="jumbotron">
+                <Container >                       
+                    <h1 className="agroworld">Agro World</h1>
+                </Container>
+            </Jumbotron>
+            
         </div>
+        
     )
 }
 
