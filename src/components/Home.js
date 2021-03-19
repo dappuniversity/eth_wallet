27e5c55092517e03566e2f5a722901f7933ba75b
Mihir <ranemihir3@gmail.com>
2021-03-19 13:16:03 +0530

    Deleted extra code in App.css

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,7 +22,7 @@ function Home() {
             <div className="statistics-strip">
                 Please visit our <a href="/statistics" className="statistics-anchor">statistics</a> page to get to know about our impact . You can donate to our project using this public key 0x7691B75d3aAED69F63d15bDe4490d926CAD94C93 .
                 <Button className="mx-4 my-2 donate-button" variant="info" onClick={routeChange}>Donate</Button> 
-                <Button className="mx-4 my-2 close" onClick={closeStrip}>&times;</Button>    
+                <Button className="mr-1 close" onClick={closeStrip}>&times;</Button>    
             </div>} 
                     
             <Jumbotron fluid className="jumbotron">
